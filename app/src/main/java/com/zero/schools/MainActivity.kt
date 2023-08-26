2193804c09bfc1c712d3e8539aabadeab77798d1
gerry <gerry>
2023-08-25 21:52:26 -0700

    test update

--- app/src/main/java/com/zero/schools/MainActivity.kt
+++ app/src/main/java/com/zero/schools/MainActivity.kt
@@ -2,7 +2,10 @@ package com.zero.schools
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val TAG = "MainActivity"
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -10,5 +13,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d(TAG, "onCreate: test.")
     }
 }
